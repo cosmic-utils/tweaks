ee313edb28eb21c5946e292f9b836ee14322a779
Eduardo Flores <edfloreshz@gmail.com>
2024-08-15 16:07:56 -0700

    improv: simplify main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use app::TweakTool;
 mod app;
 mod core;
 mod pages;
 
 fn main() -> cosmic::iced::Result {
     let settings = core::config::get();
-    cosmic::app::run::<TweakTool>(settings, ())
+    cosmic::app::run::<app::TweakTool>(settings, ())
 }
