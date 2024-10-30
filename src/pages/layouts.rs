3684c059fdb059fd2f3784de978eba17beff66d7
Eduardo Flores <edfloreshz@gmail.com>
2024-10-30 21:33:31 +0100

    improv: add button to save current layout

--- src/pages/layouts.rs
+++ src/pages/layouts.rs
@@ -7,7 +7,7 @@ use cosmic::{
 use cosmic_ext_config_templates::{generate_template, load_template};
 use dirs::data_local_dir;
 
-use crate::app::TweakTool;
+use crate::{app::TweakTool, core::icons, fl};
 
 pub mod config;
 pub mod factory;
@@ -28,6 +28,7 @@ impl Default for Layouts {
 #[derive(Debug, Clone)]
 pub enum Message {
     SelectLayout(Layout),
+    OpenSaveDialog,
     SaveCurrentLayout(String),
 }
 
@@ -48,32 +49,59 @@ impl Layouts {
             })
             .collect::<Vec<Element<Message>>>();
 
-        widget::scrollable(
-            widget::settings::section().title("Layouts").add(
-                widget::flex_row(layouts)
-                    .row_spacing(spacing.space_s)
-                    .column_spacing(spacing.space_s)
-                    .apply(widget::container)
-                    .padding([0, spacing.space_xxs]),
-            ),
-        )
+        widget::scrollable(widget::column::with_children(vec![
+            widget::row::with_children(vec![
+                widget::text::title3(fl!("layouts")).into(),
+                widget::horizontal_space().into(),
+                widget::tooltip::tooltip(
+                    icons::get_handle("arrow-into-box-symbolic", 16)
+                        .apply(widget::button::icon)
+                        .padding(spacing.space_xxs)
+                        .on_press(Message::OpenSaveDialog)
+                        .class(cosmic::style::Button::Standard),
+                    widget::text(fl!("save-current-layout")),
+                    widget::tooltip::Position::Bottom,
+                )
+                .into(),
+            ])
+            .spacing(spacing.space_xxs)
+            .into(),
+            widget::settings::section()
+                .title("Default")
+                .add(
+                    widget::flex_row(layouts)
+                        .row_spacing(spacing.space_s)
+                        .column_spacing(spacing.space_s)
+                        .apply(widget::container)
+                        .padding([0, spacing.space_xxs]),
+                )
+                .into(),
+        ]))
         .into()
     }
 
     pub fn update(&mut self, message: Message) -> Task<crate::app::Message> {
+        let mut commands = vec![];
         match message {
             Message::SelectLayout(layout) => {
                 if let Err(e) = load_template(layout.schema().clone()) {
                     eprintln!("Failed to load template: {}", e);
                 }
             }
+            Message::OpenSaveDialog => commands.push(self.update(Message::OpenSaveDialog)),
             Message::SaveCurrentLayout(name) => {
                 let path = data_local_dir()
                     .unwrap()
-                    .join("cosmic")
                     .join(TweakTool::APP_ID)
-                    .join("layouts")
-                    .join(&name);
+                    .join("layouts");
+
+                if !path.exists() {
+                    if let Err(e) = std::fs::create_dir_all(&path) {
+                        log::error!("{e}");
+                    }
+                }
+                let mut path = path.join(&name);
+                path.set_extension("ron");
                 let config_dirs = vec![
                     PathBuf::from("com.system76.CosmicPanel"),
                     PathBuf::from("com.system76.CosmicPanel.Dock"),
@@ -93,17 +121,17 @@ impl Layouts {
                             match self.config.set_layouts(helper, layouts) {
                                 Ok(written) => {
                                     if !written {
-                                        eprintln!("Failed to write layouts to config");
+                                        log::error!("Failed to write layouts to config");
                                     }
                                 }
-                                Err(e) => eprintln!("Failed to set layouts: {}", e),
+                                Err(e) => log::error!("Failed to set layouts: {}", e),
                             }
                         }
                     }
-                    Err(e) => eprintln!("Failed to generate template: {}", e),
+                    Err(e) => log::error!("Failed to generate template: {}", e),
                 }
             }
         }
-        Task::none()
+        Task::batch(commands)
     }
 }
