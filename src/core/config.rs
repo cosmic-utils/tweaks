6b9c671500db0ce189f6b885b6e47254ed3410ed
Vukašin Vojinović <wscientia+github@gmail.com>
2024-11-19 15:00:59 +0100

    improv(config): set minimum window size

--- src/core/config.rs
+++ src/core/config.rs
@@ -5,7 +5,11 @@ pub fn get() -> (cosmic::app::Settings, crate::app::Flags) {
     ICON_CACHE.get_or_init(|| Mutex::new(IconCache::new()));
     init_logger();
     (
-        cosmic::app::Settings::default(),
+        cosmic::app::Settings::default().size_limits(
+            cosmic::iced::Limits::NONE
+                .min_width(360.0)
+                .min_height(180.0),
+        ),
         crate::app::Flags {
             config_handler: crate::settings::TweaksSettings::config_handler(),
             config: crate::settings::TweaksSettings::config(),
