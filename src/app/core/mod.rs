cbb8a7a897754459230b87caa008a2c5536e89ae
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2025-09-21 11:23:46 +0200

    export macros

--- src/app/core/mod.rs
+++ src/app/core/mod.rs
@@ -3,6 +3,5 @@ pub mod error;
 pub mod grid;
 pub mod icons;
 pub mod key_bindings;
-pub mod localize;
 pub mod settings;
 pub mod style;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-mod app;
-
 pub use app::core::error::Error;
 use app::core::settings;
+
+#[allow(unused_imports)]
+#[macro_use]
+extern crate log;
+
+#[macro_use]
+pub mod localize;
+
+mod app;
 
 fn main() -> Result<(), Error> {
     settings::init()?;
